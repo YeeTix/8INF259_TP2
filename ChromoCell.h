c1f5aab76c433ae67c3c912e828f31cc1c8484ec
mkmalek <med-malek.25@hotmail.fr>
2019-03-31 10:53:37 -0400

    Add files via upload

--- ChromoCell.h
+++ ChromoCell.h
@@ -1,12 +1,25 @@
 #pragma once
 #include <iostream>
 
-class ChromoCell {
+class ChromoCell
+{
 public:
-	ChromoCell();
-	~ChromoCell();
+	int element;
+	ChromoCell *filsgauche;
+	ChromoCell *filsdroit;
+	ChromoCell(int elem, ChromoCell *g = NULL, ChromoCell *d = NULL)//constructeur avec paramètres
+	{
+		element = elem;
+		filsgauche = g;
+		filsdroit = d;
+	}
+		
+ 	~ChromoCell();
+	
+
+
 private:
 	//TODO
-
+	
 	friend std::istream& operator >> (std::istream& in, ChromoCell& cell);
 };
